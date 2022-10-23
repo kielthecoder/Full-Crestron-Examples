8718c4a5ef85053788a2e098297d42eb7579f3e5
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2022-10-23 09:50:14 -0400

    Added SGD for lobby

--- P301-Exam/Residence/ControlSystem.cs
+++ P301-Exam/Residence/ControlSystem.cs
@@ -1,14 +1,18 @@
 using System;
 using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
 using Crestron.SimplSharpPro;
 using Crestron.SimplSharpPro.CrestronThread;
 using Crestron.SimplSharpPro.Diagnostics;
 using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.UI;
 
 namespace Residence
 {
     public class ControlSystem : CrestronControlSystem
     {
+        private BasicTriListWithSmartObject _tpLobby;
+
         public ControlSystem() : base()
         {
             try
@@ -25,12 +29,22 @@ namespace Residence
         {
             try
             {
+                _tpLobby = new Tsw1052(0x03, this);
+                _tpLobby.LoadSmartObjects(Path.Combine(Directory.GetApplicationDirectory(), "Lobby_TSW-1052.sgd"));
+                _tpLobby.SigChange += _tpLobby_SigChange;
 
+                if (_tpLobby.Register() != eDeviceRegistrationUnRegistrationResponse.Success)
+                    ErrorLog.Error("Failed to register touchpanel: {0}", _tpLobby.RegistrationFailureReason);
             }
             catch (Exception e)
             {
                 ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
             }
+        }
+
+        private void _tpLobby_SigChange(BasicTriList dev, SigEventArgs args)
+        {
+
         }
     }
 }
